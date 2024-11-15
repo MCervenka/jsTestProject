a88f2f8e1ed9e4941fbc708217065faa2df835b6
Martin <martincervenka10@gmail.com>
2024-11-15 13:14:18 +0100

    first test commit

---
+++ src/testFile.ts
@@ -0,0 +1,10 @@
+//var variableOne;
+
+let variableTwo: string | boolean | number = "2";
+
+variableTwo = false;
+variableTwo = "aasdasd";
+variableTwo = 123;
+
+//boolean
+const variableThree: string = false;
